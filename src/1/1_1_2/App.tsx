66bcbe5e47e8a052bf7b1798e4da17238ac5bf71
Pronin_PO <ppronin_97@mail.ru>
2026-08-17 16:16:01 +0600

    1_1_2

--- src/1/1_1_2/App.tsx
+++ src/1/1_1_2/App.tsx
@@ -4,6 +4,7 @@
 */
 
 export default function Profile() {
-  return;
-  <img src="KatsukoSaruhashi.jpg" alt="Katsuko Saruhashi" />;
+  return (
+    <img src="KatsukoSaruhashi.jpg" alt="Katsuko Saruhashi" />
+  );
 }
