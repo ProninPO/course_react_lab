c66ed2c659ae5efad443fd1cb7d88f2b8d591825
Pronin_PO <ppronin_97@mail.ru>
2026-08-18 20:25:23 +0600

    1_7_3

--- src/1/1_7_3/App.tsx
+++ src/1/1_7_3/App.tsx
@@ -5,19 +5,23 @@
 
 import { recipes } from "./data";
 
+function Recipe({id, name, ingredients}: {id: string, name: string, ingredients: string[]}) {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <ul>
+        {ingredients.map(i => <li key={i}>{i}</li>)}
+      </ul>
+    </div>
+  );
+}
+
 export default function RecipeList() {
   return (
     <div>
       <h1>Recipes</h1>
       {recipes.map((recipe) => (
-        <div key={recipe.id}>
-          <h2>{recipe.name}</h2>
-          <ul>
-            {recipe.ingredients.map((ingredient) => (
-              <li key={ingredient}>{ingredient}</li>
-            ))}
-          </ul>
-        </div>
+        <Recipe key={recipe.id} id={recipe.id} name={recipe.name} ingredients={recipe.ingredients} />
       ))}
     </div>
   );
