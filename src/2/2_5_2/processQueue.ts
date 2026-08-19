312026d4aa41ca4b13a4c861639650c56d16bacb
Pronin_PO <ppronin_97@mail.ru>
2026-08-19 16:03:18 +0600

    2_5_2

--- src/2/2_5_2/processQueue.ts
+++ src/2/2_5_2/processQueue.ts
@@ -4,6 +4,14 @@ export function getFinalState(baseState: number, queue: Que[]) {
     let finalState = baseState;
 
     // TODO: do something with the queue...
+    for (let item of queue) {
+        if (typeof item === 'function') {
+            finalState = item(finalState);
+        }
+        else {
+            finalState = item;
+        }
+    }
 
     return finalState;
 }
