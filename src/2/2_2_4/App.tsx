3b70ab794480b1eb6a1a5f0fd4d0e4afc4c3acc3
Pronin_PO <ppronin_97@mail.ru>
2026-08-19 11:50:06 +0600

    2_2_4

--- src/2/2_2_4/App.tsx
+++ src/2/2_2_4/App.tsx
@@ -7,13 +7,13 @@
   Можете ли вы объяснить, почему эта переменная состояния была ненужной?
 */
 
-import { useState } from 'react';
+//import { useState } from 'react';
 
 export default function FeedbackForm() {
-    const [name, setName] = useState('');
+    let name: string = '';
 
     function handleClick() {
-        setName(prompt('What is your name?')??"");
+        name = prompt('What is your name?') ?? "";
         alert(`Hello, ${name}!`);
     }
 
