bafa6e22ef1e9554dcec9ede437b7b0b834cc63d
Pronin_PO <ppronin_97@mail.ru>
2026-08-20 21:33:58 +0600

    3_4_1

--- src/3/3_4_1/App.tsx
+++ src/3/3_4_1/App.tsx
@@ -7,23 +7,16 @@ import { useState } from 'react';
 
 export default function App() {
   const [showHint, setShowHint] = useState(false);
-  if (showHint) {
-    return (
-      <div>
-        <p><i>Hint: Your favorite city?</i></p>
-        <Form />
-        <button onClick={() => {
-          setShowHint(false);
-        }}>Hide hint</button>
-      </div>
-    );
-  }
+
   return (
     <div>
+      {showHint && <p><i>Hint: Your favorite city?</i></p>}
       <Form />
-      <button onClick={() => {
-        setShowHint(true);
-      }}>Show hint</button>
+      {showHint ? (
+        <button onClick={() => setShowHint(false)}>Hide hint</button>
+      ) : (
+        <button onClick={() => setShowHint(true)}>Show hint</button>
+      )}
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import './3/3_3_2/style.css'
-import App from './3/3_3_2/App.tsx'
+import './3/3_4_1/style.css'
+import App from './3/3_4_1/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
