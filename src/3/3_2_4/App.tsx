4cb09afc2c7d9c0cd3279cc56fbd82eb2d587932
Pronin_PO <ppronin_97@mail.ru>
2026-08-20 14:54:24 +0600

    3_2_4

--- src/3/3_2_4/App.tsx
+++ src/3/3_2_4/App.tsx
@@ -11,14 +11,21 @@ import { letters } from './data.js';
 import Letter from './Letter.js';
 
 export default function MailClient() {
-  const [selectedId, setSelectedId] = useState<number | null>(null);
+  const [selectedIds, setSelectedIds] = useState<number[]>([]);
 
   // TODO: allow multiple selection
-  const selectedCount = 1;
+  const selectedCount = selectedIds.length;
 
   function handleToggle(toggledId: number) {
     // TODO: allow multiple selection
-    setSelectedId(toggledId);
+    if (selectedIds.includes(toggledId)) {
+      setSelectedIds(selectedIds.filter(id => id !== toggledId))
+    } else {
+      setSelectedIds([
+        ...selectedIds,
+        toggledId
+      ]);
+    }
   }
 
   return (
@@ -31,7 +38,7 @@ export default function MailClient() {
             letter={letter}
             isSelected={
               // TODO: allow multiple selection
-              letter.id === selectedId
+              selectedIds.includes(letter.id)
             }
             onToggle={handleToggle}
           />
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import './3/3_2_3/style.css'
-import App from './3/3_2_3/App.tsx'
+import './3/3_2_4/style.css'
+import App from './3/3_2_4/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
