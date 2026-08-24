df083cb63e5408009ec94f939d5642c6a87dcc10
Pronin_PO <ppronin_97@mail.ru>
2026-08-24 16:34:14 +0600

    4_4_3

--- src/4/4_4_3/EditContact.tsx
+++ src/4/4_4_3/EditContact.tsx
@@ -8,13 +8,12 @@ export default function EditContact({
   savedContact: ContactType;
   onSave: (updatedData: ContactType) => void;
 }) {
+  return <EditForm savedContact={savedContact} onSave={onSave} key={savedContact.id} />;
+}
+
+function EditForm({ savedContact, onSave }: { savedContact: ContactType; onSave: (updatedData: ContactType) => void; }) {
   const [name, setName] = useState(savedContact.name);
   const [email, setEmail] = useState(savedContact.email);
-
-  useEffect(() => {
-    setName(savedContact.name);
-    setEmail(savedContact.email);
-  }, [savedContact]);
 
   return (
     <section>
