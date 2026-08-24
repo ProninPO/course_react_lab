8360e49b5a628d1a76df029e486f63f50d18c3e5
Pronin_PO <ppronin_97@mail.ru>
2026-08-24 16:38:59 +0600

    4_4_4

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import './4/4_4_3/style.css'
-import App from './4/4_4_3/App.tsx'
+import './4/4_4_4/style.css'
+import App from './4/4_4_4/App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
