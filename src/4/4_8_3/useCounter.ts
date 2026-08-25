b23221258ec9f94149e3162ca53d88dcf3a6323d
Pronin_PO <ppronin_97@mail.ru>
2026-08-25 14:35:53 +0600

    4_8_3

--- src/4/4_8_3/useCounter.ts
+++ src/4/4_8_3/useCounter.ts
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
+import { useInterval } from './useInterval';
 
-export function useCounter(delay) {
+export function useCounter(delay: number) {
     const [count, setCount] = useState(0);
     useInterval(() => {
         setCount((c) => c + 1);
--- src/4/4_8_3/useInterval.ts
+++ src/4/4_8_3/useInterval.ts
@@ -2,4 +2,10 @@ import { useEffect } from "react";
 
 export function useInterval(callback: () => void, delay: number) {
     // Тут нужно реализовать хук
+    useEffect(() => {
+        const id = setInterval(callback, delay);
+        return () => {
+            clearInterval(id);
+        };
+    }, [callback, delay]);
 }
