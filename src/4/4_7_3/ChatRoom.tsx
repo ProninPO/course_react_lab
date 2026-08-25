4e6865f7d7c49953e72cb4bb2305d7aff505c430
Pronin_PO <ppronin_97@mail.ru>
2026-08-25 11:16:33 +0600

    4_7_3

--- src/4/4_7_3/ChatRoom.tsx
+++ src/4/4_7_3/ChatRoom.tsx
@@ -2,11 +2,13 @@ import { useEffect } from 'react';
 import { createConnection, Options } from './chat';
 
 export default function ChatRoom({ options }: { options: Options }) {
+  const { serverUrl, roomId } = options;
+
   useEffect(() => {
-    const connection = createConnection(options);
+    const connection = createConnection({ serverUrl: serverUrl, roomId: roomId });
     connection.connect();
     return () => connection.disconnect();
-  }, [options]);
+  }, [serverUrl, roomId]);
 
   return <h1>Welcome to the {options.roomId} room!</h1>;
 }
